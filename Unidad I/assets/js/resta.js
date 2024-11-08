dd79a1de581c040c4f942e3270ac0d5e7584b5a6
Cristoffer Venega D. <cvenegad@ccaf.andes>
2024-11-08 14:20:48 -0300

    Avance ejercicion Unidad I

---
+++ Unidad I/assets/js/resta.js
@@ -0,0 +1,9 @@
+
+
+num1 = prompt("Ingrese el primer número")
+num2 = prompt("Ingrese el segundo número")
+resultado = Number(num1) - Number(num2)
+
+item1 = document.querySelector("#valor")
+item1.innerHTML = resultado
+
