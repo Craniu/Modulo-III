94579595ef7645659c35509d4761d597fe5a5b5c
Cristoffer Venega D. <cvenegad@ccaf.andes>
2024-11-08 11:39:30 -0300

    finalizacion clase Unidad I

---
+++ Unidad I/assets/js/scripts.js
@@ -0,0 +1,35 @@
+// alert("aprende haciendo");
+
+// numero1 = 100
+// numero2 = 50
+// alert(numero1 + numero2)
+
+// nombre = prompt("Ingrese su nombre")
+// alert ("hola " + nombre)
+
+// num1 = prompt("Ingrese el primer número")
+// num2 = prompt("Ingrese el segundo número")
+// resultado = Number(num1) + Number(num2)
+// alert ("El resultado es: " + resultado)
+
+
+// menu = document.querySelector(".menu")
+// menu.innerHTML = ("Menu 2.0")
+// heroSection = document.querySelector("#Hero-Section")
+// pieDePagina = document.querySelector("footer")
+// titulo = document.querySelector("h1")
+// titulo.innerHTML = "Este contenido fue agregado con JavaScript"
+
+// nombre = prompt("Ingrese su nombre:")
+// nacionalidad = prompt("Ingrese su nacionalidad:")
+// ele1 = document.querySelector("#info1")
+// ele2 = document.querySelector("#info2")
+// ele1.innerHTML = nombre
+// ele2.innerHTML = nacionalidad
+
+// var titulo = document.querySelector("h3")
+// titulo.style.color = "#7ba238"
+
+borde = prompt("Ingrese el % de radio requerido")
+imagen = document.querySelector("img")
+imagen.style.borderRadius = borde + "%"
