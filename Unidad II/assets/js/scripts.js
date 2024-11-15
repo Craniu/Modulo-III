7c0f6f369da0f8c2eaff26e741d3981e9d8459fd
Cristoffer Venega D. <cvenegad@ccaf.andes>
2024-11-14 21:47:25 -0300

    add guia de estudios

--- Unidad II/assets/js/scripts.js
+++ Unidad II/assets/js/scripts.js
@@ -1,35 +1,44 @@
-// alert("aprende haciendo");
+// grados = Number(prompt("Introduce la temperatura"))
+// if (grados > 30) {
+//     console.log('Uf que Calor!')
+// }
 
-// numero1 = 100
-// numero2 = 50
-// alert(numero1 + numero2)
+btn = document.querySelector("#btn")
 
-// nombre = prompt("Ingrese su nombre")
-// alert ("hola " + nombre)
+btn.addEventListener("click",()=>{
+    value = document.querySelector('#input').value
+    if (value == 'password-secreto'){
+        par = document.querySelector('#secret-result')
+        par.innerHTML = 'lo lograste'
+        }else{
+           par = document.querySelector('#secret-result')
+           par.innerHTML = 'Nones'
+        }
+})
 
-// num1 = prompt("Ingrese el primer número")
-// num2 = prompt("Ingrese el segundo número")
-// resultado = Number(num1) + Number(num2)
-// alert ("El resultado es: " + resultado)
+if (false) {
+    console.log("Este mensaje no se mostrará");
+    } else if (false) {
+    console.log("Este mensaje tampoco");
+    } else if (true) {
+    console.log("Este mensaje SI se mostrará 👋😁");
+    } else {
+    console.log("A mi no me verás 😑🙄");
+    }
+    
 
+    btn2 = document.querySelector("#btnVer")
+    btn2.addEventListener("click", () => {
+        ancho = Number(document.querySelector("#inAncho").value)
+        if(ancho < 576){
+            alert("xs")
+        }else if(ancho >= 576 && ancho < 768){
+            alert("sm")
+        }else if(ancho >=768 && ancho < 962){
+            alert("md")
+        }else if(ancho >= 962){
+            alert("xl")
+        }
+    })
 
-// menu = document.querySelector(".menu")
-// menu.innerHTML = ("Menu 2.0")
-// heroSection = document.querySelector("#Hero-Section")
-// pieDePagina = document.querySelector("footer")
-// titulo = document.querySelector("h1")
-// titulo.innerHTML = "Este contenido fue agregado con JavaScript"
-
-// nombre = prompt("Ingrese su nombre:")
-// nacionalidad = prompt("Ingrese su nacionalidad:")
-// ele1 = document.querySelector("#info1")
-// ele2 = document.querySelector("#info2")
-// ele1.innerHTML = nombre
-// ele2.innerHTML = nacionalidad
-
-// var titulo = document.querySelector("h3")
-// titulo.style.color = "#7ba238"
-
-borde = prompt("Ingrese el % de radio requerido")
-imagen = document.querySelector("img")
-imagen.style.borderRadius = borde + "%"
+    
