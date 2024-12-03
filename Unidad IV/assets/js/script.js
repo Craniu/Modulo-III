e81c5f43b6c753ad0d0321279754590c9a399348
Cristoffer Venega D. <cvenegad@ccaf.andes>
2024-12-03 16:06:54 -0300

    ini Mod IV

---
+++ Unidad IV/assets/js/script.js
@@ -0,0 +1,18 @@
+const nombres = [  "Juan",  "Luisa",  "Fabian",  "Jorge", ]
+for (let nombre of nombres){{
+    console.log(nombre);
+}}
+
+const lenguajes = [  "JavaScript",  "Php",  "Ruby",  "Python",  "Scala",  "Go" ]
+for (let lenguaje of lenguajes) { console.log(lenguaje) }
+
+
+const nombre = "Luis";
+const fecha = "2022-03-25";
+const interpolacion = `Hola, mi nombre es ${nombre}, hoy ${fecha} quiero emitir una solicitud`;
+const concatenacion = 'Hola, mi nombre es ' + nombre + ', hoy ' + fecha + ' quiero emitir una solicitud' ;
+console.log(interpolacion) 
+console.log(concatenacion)
+
+
+const escalaMayorDeDo= [  "Do",  "Re",  "Mi",  "Fa",  "Sol",  "La",  "Si", ]
